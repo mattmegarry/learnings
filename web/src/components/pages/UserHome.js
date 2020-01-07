2ad1e58592008c194ba9c469034d39c6f5b398a7
Matt Megarry <mattmegarry@gmail.com>
2020-01-07 13:06:33 +0000

    Fix recent snippets not updating upon quick add

--- web/src/components/pages/UserHome.js
+++ web/src/components/pages/UserHome.js
@@ -3,21 +3,48 @@ import React, { Component } from "react";
 import QuickAddSnippet from "../blocks/QuickAddSnippet";
 import SnippetsRecent from "../blocks/SnippetsRecent";
 
+import { authRequest } from "../../utils/http.utils";
+
 class UserHome extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      snippets: []
+    };
+
+    this.getRecentSnippets = this.getRecentSnippets.bind(this);
+  }
+
+  getRecentSnippets() {
+    const { userId } = this.props;
+    if (userId) {
+      authRequest(`/users/${userId}/recent-snippets`, "GET")
+        .then(res => {
+          if (res.status === 200) {
+            this.setState({ snippets: res.data.snippets });
+          }
+        })
+        .catch(e => console.error(e));
+    }
   }
 
   render() {
     const userId = this.props.userId;
+    const { snippets } = this.state;
+    const getRecentSnippets = this.getRecentSnippets;
 
     return (
       <>
         <QuickAddSnippet
           userId={userId}
-          getRecentSnippets={this.getRecentSnippets}
+          getRecentSnippets={getRecentSnippets}
         />
-        <SnippetsRecent userId={userId} />
+        <SnippetsRecent
+          userId={userId}
+          getRecentSnippets={getRecentSnippets}
+          snippets={snippets}
+        />
       </>
     );
   }
